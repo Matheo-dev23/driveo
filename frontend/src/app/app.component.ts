89f49fbf47c7efefcf35e66173eec92f3fff079a
Paszkowski Matheo <matheo.paszkowski.etu>
2026-01-09 23:56:16 +0100

    feat: render list HTML of two elements ProductCardComponents in AppComponent - AppComponent take an attribute of type Product initialized in the constructor - This attribute is passed in one ProductComponent, and the other one is create with default values

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ProductCardComponent} from './components/product-card/product-card.component';
+import {Product} from './models/product.model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: ""
+  templateUrl: './app.component.html',
+  imports: [
+    ProductCardComponent
+  ],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  product1 : Product;
+
+  constructor() {
+    this.product1  = new Product();
+    this.product1.title = "Prince";
+    this.product1.description = "Biscuits fourrés goût vanille au blé complet ouverture facile ";
+    this.product1.price = 1.85;
+    this.product1.imgUrl = "https://cdn.auchan.fr/media/A0220150721000894551PRIMARY_1024x1024/B2CD/"
+  }
 
 }
