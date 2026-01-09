99c991a4f04cf1af56e8b941d00c1c9b7467d7eb
Paszkowski Matheo <matheo.paszkowski.etu>
2026-01-09 23:51:09 +0100

    feat: create product model to represent an object of type Product() that take in attribute : - title: String - description: String - price: number - imgUrl : string

---
+++ frontend/src/app/models/product.model.ts
@@ -0,0 +1,8 @@
+export class Product {
+
+  title: string = "Product";
+  description: string = "Product description";
+  imgUrl: string = "https://www.drivezeclerc.re/portail-st-leu/21235-thickbox_default/biscuit-multicereales-gout-chocolat-x15-prince-lu-293grs.jpg";
+  price: number = 9.99;
+
+}
